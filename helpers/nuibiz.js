eabb4041d98ecfa08709a530eba9da1217fcf063
vieri1 <vieriflores2002xdxd@gmail.com>
2024-12-30 20:17:39 -0500

    :0

--- helpers/nuibiz.js
+++ helpers/nuibiz.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
-const {BASE_URL,PASSWORD_NUIBIZ,USER_NUIBIZ,ECOMMERCE_URL,merchantID} = process.env;
+require("dotenv").config({path:"../.env"});
+console.log("process.env:", process.env);
+const {BASE_URL,PASSWORD_NUIBIZ,USER_NUIBIZ,ECOMMERCE_URL,merchantID,PORT_CANCHAS} = process.env;
 const axios = require('axios');
-console.log(BASE_URL,USER_NUIBIZ);
+console.log(BASE_URL,USER_NUIBIZ,PASSWORD_NUIBIZ,PORT_CANCHAS);
 
 // Codificar credenciales en Base64
 const encodedCredentials = Buffer.from(`${USER_NUIBIZ}:${PASSWORD_NUIBIZ}`).toString('base64');
